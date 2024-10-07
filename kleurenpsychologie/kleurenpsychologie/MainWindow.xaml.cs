f07c8c386896a49cbb4ffa06da232caf4f5bf52f
MichaiLionakisPXL <Michail.Lionakis@STUDENT.PXL.BE>
2024-10-07 20:53:12 +0200

    KLEUR-03

--- kleurenpsychologie/kleurenpsychologie/MainWindow.xaml.cs
+++ kleurenpsychologie/kleurenpsychologie/MainWindow.xaml.cs
@@ -19,6 +19,57 @@ namespace kleurenpsychologie
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void pxlPng_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Black);
+        }
+
+        private void pxlPng_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void geelPng_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFF00"));
+        }
+
+        private void geelPng_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void roodPng_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void roodPng_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void groenPng_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Green);
+        }
+
+        private void groenPng_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void blauwPng_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void blauwPng_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
         }
     }
 }
